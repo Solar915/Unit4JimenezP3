f6d2b99be7281d4ea58284d3f97e19bc0ccc07f3
Ethan Jimenez <145072456+Solar915@users.noreply.github.com>
2024-08-30 10:22:57 -0700

    Im finishing on camera position
    I'm currently working on fixing my camera position

--- Unit4P7Jimenez/Assets/Scripts/RotateCamera.cs
+++ Unit4P7Jimenez/Assets/Scripts/RotateCamera.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class RotateCamera : MonoBehaviour
 {
+    public float rotationSpeed;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,6 @@ public class RotateCamera : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(Vector3.up, 1);
     }
 }
